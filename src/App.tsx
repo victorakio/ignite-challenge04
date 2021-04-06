f9c2f537f41de48511f5219fa9386db9c3498f5e
Victor Akio <victorakioz@gmail.com>
2021-04-06 20:16:40 -0300

    Convert whole aplication to typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Routes from "./routes";
+
+import GlobalStyle from "./styles/global";
+
+const App = (): JSX.Element => (
+  <>
+    <GlobalStyle />
+    <Router>
+      <Routes />
+    </Router>
+  </>
+);
+
+export default App;
